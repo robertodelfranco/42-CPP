d6d5ef2de54d99e0d9a7485d6c6ca4c40da5aa9d
Roberto Del Franco <robertinhodf@gmail.com>
2025-08-24 20:00:38 -0300

    ex04 done !

---
+++ CPP01/ex04/replace.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 19:18:23 by marvin            #+#    #+#             */
+/*   Updated: 2025/08/24 19:18:23 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "replace.hpp"
+
+void	stringReplace(std::string filename, std::string s1, std::string s2) {
+	if (s1.empty()) {
+		std::cout << "S1 cannot be empty!" << std::endl;
+		std::exit(1);
+	}
+
+	std::ifstream infile(filename.c_str());
+	if (!infile) {
+		std::cout << "Error ao abrir arquivo de entrada!" << std::endl;
+		std::exit(1);
+	}
+
+	std::ofstream outfile((filename + ".replace").c_str());
+	if (!outfile) {
+		std::cout << "Error ao abrir arquivo de saída!" << std::endl;
+		std::exit(1);
+	}
+	
+	std::string	line;
+	while (std::getline(infile, line)) {
+		size_t	pos = 0;
+		while (true) {
+			size_t	find = line.find(s1, pos);
+			if (find == std::string::npos) {
+				outfile << line.substr(pos);
+				break;
+			} else {
+				outfile << line.substr(pos, find - pos);
+				outfile << s2;
+				pos = find + s1.length();
+			}
+		}
+	}
+	infile.close();
+	outfile.close();
+}
---
+++ CPP01/ex04/replace.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/24 19:12:19 by marvin            #+#    #+#             */
+/*   Updated: 2025/08/24 19:12:19 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+# define REPLACE_HPP
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+void	stringReplace(std::string filename, std::string s1, std::string s2);
+
+#endif /* REPLACE_HPP */
