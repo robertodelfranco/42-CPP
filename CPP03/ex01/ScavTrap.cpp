08b8be3c5bbae0ea85f49ea9e2c7050f36cf7e34
Roberto Del Franco <robertinhodf@gmail.com>
2025-08-28 16:01:42 -0300

    att ex01 to be more readable

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 12:09:52 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/08/28 14:08:51 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/08/28 16:00:13 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ ScavTrap::ScavTrap(const std::string name) : ClapTrap(name) {
 	m_hitPoints = 100;
 	m_energy = 50;
 	m_attackDamage = 20;
-	std::cout << GREEN << "ScavTrap default constructor called" << NC << std::endl;	
+	std::cout << CYAN << "ScavTrap default constructor called" << NC << std::endl;	
 }
 
 ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other) {
@@ -31,20 +31,20 @@ ScavTrap&	ScavTrap::operator=(const ScavTrap& other) {
 }
 
 ScavTrap::~ScavTrap() {
-	std::cout << RED << "ScavTrap destructor called" << NC << std::endl;
+	std::cout << CYAN << "ScavTrap destructor called" << NC << std::endl;
 }
 
 void	ScavTrap::attack(const std::string& target) {
 	if (m_hitPoints > 0 && m_energy > 0) {
 		m_energy -= 1;
-		std::cout << YELLOW << "ScavTrap " << m_name << " attacks " << target << ", causing " << m_attackDamage << " points of damage!" << NC << std::endl;
+		std::cout << CYAN << "ScavTrap " << m_name << " attacks " << target << ", causing " << m_attackDamage << " points of damage!" << NC << std::endl;
 	}
 	else if (m_hitPoints == 0)
-		std::cout << RED << "ScavTrap " << m_name << " cannot attack because is already dead!" << NC << std::endl;
+		std::cout << CYAN << "ScavTrap " << m_name << " cannot attack because is already dead!" << NC << std::endl;
 	else
-		std::cout << RED << "ScavTrap " << m_name << " has no energy to attack!" << NC << std::endl;
+		std::cout << CYAN << "ScavTrap " << m_name << " has no energy to attack!" << NC << std::endl;
 }
 
 void	ScavTrap::guardGate() {
-	std::cout << YELLOW << "ScavTrap " << m_name << " is now in Gate keeper mode!" << NC << std::endl;
+	std::cout << CYAN << "ScavTrap " << m_name << " is now in Gate keeper mode!" << NC << std::endl;
 }
--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 18:09:24 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/08/28 14:08:15 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/08/28 15:59:30 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,6 @@ int main() {
 	ClapTrap b("Jane doe");
 
 	std::cout << std::endl;
-	
-	a.setAttackDamage(3);
-	a.setAttackDamage(-3);
-	b.setAttackDamage(2);
-
-	std::cout << std::endl;
-	
-	a.attack("Jane doe");
-	a.takeDamage(-9);
-	b.takeDamage(a.getAttackDamage());
-	b.attack("John");
-	a.takeDamage(b.getAttackDamage());
-	b.beRepaired(1);
-	b.beRepaired(-1);
-	a.attack("Jane doe");
-	b.takeDamage(a.getAttackDamage());
-	b.attack("John");
-	b.beRepaired(8);
-	a.attack("Jane doe");
-	b.takeDamage(a.getAttackDamage());
-	a.beRepaired(2);
-	b.beRepaired(5);
 
 	ScavTrap s("Scav");
 
