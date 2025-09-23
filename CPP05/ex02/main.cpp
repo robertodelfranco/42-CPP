d3f67b231fa77b8359ba16120b20d7a0f775fb50
Roberto Del Franco <robertinhodf@gmail.com>
2025-09-23 11:16:10 -0300

    little adjust on main

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 14:21:00 by rdel-fra          #+#    #+#             */
-/*   Updated: 2025/09/23 11:08:29 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2025/09/23 11:15:45 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 #include "PresidentialPardonForm.hpp"
 
 int main() {
-    Bureaucrat john("John", 1);
-    Bureaucrat jane("Jane", 150);
+    Bureaucrat john("John", 5);
+    Bureaucrat jane("Jane", 137);
 
     RobotomyRequestForm robotomyForm("RobotomyTarget");
     ShrubberyCreationForm shrubberyForm("ShrubberyTarget");
     PresidentialPardonForm pardonForm("PardonTarget");
 
     std::cout << "\n--- Signing Forms ---\n" << std::endl;
-    john.signForm(robotomyForm);
     jane.signForm(robotomyForm);
+    john.signForm(robotomyForm);
     jane.signForm(shrubberyForm);
     john.signForm(shrubberyForm);
     jane.signForm(pardonForm);
